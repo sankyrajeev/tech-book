fdb1fc2fac8f614f4d2c968a9f1d0a8a95d92559
Vontell <sankarsh.rajeev@gmail.com>
2022-11-21 09:24:51 -0800

    first files added

---
+++ config/connection.js
@@ -0,0 +1,14 @@
+const Sequelize = require('sequelize');
+
+require('dotenv').config();
+
+// creating connection to our db
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    });
+
+module.exports = sequelize;
